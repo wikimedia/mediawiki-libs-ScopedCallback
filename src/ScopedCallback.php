3b8701b5c549c85f370894ee48603975ff4bb66f
Reedy <reedy@wikimedia.org>
2020-11-12 10:41:56 +0000

    Add @codeCoverageIgnore to ScopedCallback::newScopedIgnoreUserAbort()
    Code is a no-op in CLI, which is how CI is executed.
    
    Change-Id: Ia05235a22e5d87fe08f3efa54a5f6ab0ca9cfb74

--- src/ScopedCallback.php
+++ src/ScopedCallback.php
@@ -74,8 +74,12 @@ class ScopedCallback {
 	 *
 	 * @since 3.0.0
 	 * @return ScopedCallback|null
+	 *
+	 * @codeCoverageIgnore CI is only run via CLI, so this will never be exercised.
+	 * Also no benefit testing a function just returns null.
 	 */
 	public static function newScopedIgnoreUserAbort() {
+		// ignore_user_abort previously caused an infinite loop on CLI
 		// https://bugs.php.net/bug.php?id=47540
 		if ( PHP_SAPI != 'cli' ) {
 			// avoid half-finished operations
